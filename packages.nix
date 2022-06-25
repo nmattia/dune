860ed4a3753439eed2a680bc8a209ccd104fba5f
Nicolas Mattia <nicolas@nmattia.com>
2022-06-25 11:15:28 +0200

    Add ffmpeg

--- packages.nix
+++ packages.nix
@@ -1,7 +1,20 @@
+let lib = import ./lib.nix;
+in
 {
   nodejs =
     let
       npm-src = builtins.fetchTarball https://nodejs.org/download/release/v16.14.0/node-v16.14.0-darwin-x64.tar.gz;
     in
     { bin = "${npm-src}/bin"; };
+
+
+    ffmpeg =
+      let ffmpegZip = builtins.fetchurl https://evermeet.cx/ffmpeg/ffmpeg-5.0.1.zip;
+      ffmpeg = lib.runCommand "ffmpeg" {}''
+        export PATH=/usr/bin:/bin
+        mkdir -p $out/bin
+        unzip ${ffmpegZip} -d $out/bin
+        '';
+      in
+      { bin = "${ffmpeg}/bin"; };
 }
