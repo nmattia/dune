4b4a888c98d6f05639e085e4d2f06446e8b3cea9
Nicolas Mattia <nicolas@nmattia.com>
2023-01-04 13:53:29 +0100

    Add avr-gcc and avr-libc

--- lib.nix
+++ lib.nix
@@ -5,9 +5,8 @@
       rec {
         inherit name;
         builder = "/bin/bash";
-        args = [ "-euo" "pipefail" "-c" input ];
+        args = [ "-euo" "pipefail" "-c" cmd ];
         system = builtins.currentSystem;
-        input = cmd;
       } // env
     );
 
