e4d153d5508bab32f1974e03f833091bfe7fa2cf
Nicolas Mattia <nicolas@nmattia.com>
2023-11-15 13:00:10 +0100

    Bump nodejs to 20.9.0

--- packages.nix
+++ packages.nix
@@ -36,7 +36,7 @@ rec
 {
   nodejs =
     let
-      npm-src = builtins.fetchTarball https://nodejs.org/download/release/v16.14.0/node-v16.14.0-darwin-x64.tar.gz;
+      npm-src = builtins.fetchTarball https://nodejs.org/download/release/v20.9.0/node-v20.9.0-darwin-x64.tar.gz;
     in
     { bin = "${npm-src}/bin"; };
 
